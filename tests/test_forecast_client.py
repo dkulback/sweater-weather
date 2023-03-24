142b9b28debc14434216546fa66165c409ba5c4c
Darren Kulback <dkulback@gmail.com>
2023-03-23 20:27:45 -0600

    Remove parsing / change route

--- tests/test_forecast_client.py
+++ tests/test_forecast_client.py
@@ -6,8 +6,18 @@ from app.clients.forecast_client import ForecastClient
 def test_get_forecast():
     client = ForecastClient()
 
-    forecast = client.get_forecast("New York")
+    forecast = client.get_forecast(40.730610, -73.935242)
 
     assert isinstance(forecast, dict)
-    assert forecast["city"] == "New York"
-    assert "forecast" in forecast
+    assert "current" in forecast
+    assert "temp" in forecast["current"]
+    assert "feels_like" in forecast["current"]
+    assert "weather" in forecast["current"]
+    assert "description" in forecast["current"]["weather"][0]
+    assert "daily" in forecast
+    assert "temp" in forecast["daily"][0]
+    assert "min" in forecast["daily"][0]["temp"]
+    assert "max" in forecast["daily"][0]["temp"]
+    assert "weather" in forecast["daily"][0]
+
+
